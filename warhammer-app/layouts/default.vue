627b0c53ce9dec7c18b5d55e1f5fd2d68e80a3f2
SteFowles-White <fowleswhite@gmail.com>
2021-11-14 22:45:58 +0000

    updating the intial code

---
+++ warhammer-app/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <TheHeader/>
+        <Nuxt/>
+        <TheFooter/>
+    </div>
+</template>
+<script>
+import TheHeader from '../components/layout/TheHeader.vue';
+import TheFooter from '../components/layout/TheFooter.vue';
+export default {
+    components: {
+        TheHeader,
+        TheFooter
+    }
+    
+}
+</script>
---
+++ warhammer-app/pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <main>
+    <section class="w-full bg-red-900">
+      <div class="flex justify-between">
+        <header class="flex items-center content-center w-5/12">
+          <h1 class="text-8xl ml-8">War of the Gods</h1>
+        </header>
+        <picture class="w-7/12 float-right rounded-tl-full">
+          <source class="float-right rounded-tl-full" media="(min-width:650px)" srcset="../static/images/Home-Battle-Image.jpg">
+          <img
+            class="float-right rounded-tl-full"
+            src="../static/images/Home-Battle-Image-mobile.jpg"
+            alt="Epic battle with large armies"
+          />
+        </picture>
+        </div>
+    </section>
+    <section class="container mx-auto pt-10">
+      <header class="text-center pt-5 pb-5">
+        <h2 class="text-4xl">Entering the World of Chaos & War</h2>
+      </header>
+      <p class="pt-3 tb-3 text-xl">
+        This is a world of heros, Kings and Empires all fighting for different
+        reason. At the begining of time the valar created a paradise of a world.
+        Trees grew, the birds and animals thrived. Humans entere the world,
+        primative in there knoweldge and content with a plentiful world provided
+        by the valar and the Gods themselves, the Ilumita. For years they have
+        built a world for all to enjoy and all to use, the Gods walked freely
+        with the people bringing light and knowldge beyond compentions for the
+        humans.
+      </p>
+      <p class="pt-3 tb-3 text-xl">
+        It was in the year 1000 of the age of creation that a valar started to
+        turn. This valar was named Melika, he was buetiful beyond compare, long
+        blond/silver hair that flowed through the wind. Strenght, power and
+        light shone form home such as the sun broke throught the darkness of the
+        sun. However, many years in the age of creation had occoured and melkor
+        grew ideas of his own.....
+      </p>
+      <a href="#">Find out about the history of creation</a>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
